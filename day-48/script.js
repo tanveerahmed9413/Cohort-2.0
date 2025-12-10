4a6ec72bd2c243194c22d714dfb7f734fb61500a
tanveerahmed9413 <tanveerahmed4872@gmail.com>
2025-12-10 17:25:47 +0530

    Class and Object

---
+++ day-48/script.js
@@ -0,0 +1,24 @@
+class Human{
+    constructor(name,age,address){
+        this.name = name
+        this.age = age
+        this.address = address
+    }
+}
+
+
+class Beskit{
+   constructor(name,price,company,quantity){
+    this.name = name
+    this.price = price
+    this.company = company
+    this.quantity = quantity
+   }
+}
+
+let m1 = new Beskit('parle',50,'parle-g',10)
+
+
+
+ let p = new Human('nitesh',25,'dahra')
+
